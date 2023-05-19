46b59969a6159dbed5e8839165c9a8c4e4ace2ce
nemoryoliver <nemoryoliver@gmail.com>
2023-05-20 05:40:57 +0800

    updated notification icon

--- lib/notifications/notifications.manager.dart
+++ lib/notifications/notifications.manager.dart
@@ -17,7 +17,7 @@ class NotificationsManager {
       onDidReceiveLocalNotification: onForegroundPayload,
     );
 
-    const androidSettings = AndroidInitializationSettings('ic_notification');
+    const androidSettings = AndroidInitializationSettings('notification_icon');
 
     // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     await plugin.initialize(
