0728a2e269273157cd79ee65d59c581b362f27d4
nemoryoliver <nemoryoliver@gmail.com>
2024-02-26 08:01:54 +0800

    updated remote image

--- lib/widgets/remote_image.widget.dart
+++ lib/widgets/remote_image.widget.dart
@@ -1,11 +1,12 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class RemoteImage extends StatelessWidget {
   final String url;
   final double? width, height;
   final Alignment alignment;
-  final Image? placeholder;
+  final Widget? failWidget;
 
   const RemoteImage({
     Key? key,
@@ -13,19 +14,30 @@ class RemoteImage extends StatelessWidget {
     this.width,
     this.height,
     this.alignment = Alignment.center,
-    this.placeholder,
+    this.failWidget,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ExtendedImage.network(
       url,
+      cache: true,
+      fit: BoxFit.cover,
       width: width ?? height,
       height: height ?? width,
       alignment: alignment,
-      cache: true,
-      fit: BoxFit.cover,
-      enableLoadState: false,
+      enableLoadState: true,
+      loadStateChanged: (state) {
+        if (state.extendedImageLoadState == LoadState.loading) {
+          // return null;
+        } else if (state.extendedImageLoadState == LoadState.completed) {
+          return state.completedWidget.animate().fade();
+        } else if (state.extendedImageLoadState == LoadState.failed) {
+          if (failWidget != null) return failWidget;
+        }
+
+        return SizedBox.shrink();
+      },
     );
   }
 }
