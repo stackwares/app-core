a85d4eea955bbcc919bda366556b88e37fe31c55
nemoryoliver <nemoryoliver@gmail.com>
2024-03-03 06:16:36 +0800

    html image fallback

--- lib/widgets/remote_image.widget.dart
+++ lib/widgets/remote_image.widget.dart
@@ -1,14 +1,18 @@
+import 'package:console_mixin/console_mixin.dart';
 import 'package:extended_image/extended_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:image_network/image_network.dart';
 
-class RemoteImage extends StatelessWidget {
+class RemoteImage extends StatelessWidget with ConsoleMixin {
   final String url;
   final double? width, height;
   final Alignment alignment;
   final Widget? failWidget;
   final Widget? loadingWidget;
   final String? cacheKey;
+  final bool retry;
 
   const RemoteImage({
     Key? key,
@@ -19,10 +23,15 @@ class RemoteImage extends StatelessWidget {
     this.loadingWidget,
     this.failWidget,
     this.cacheKey,
+    this.retry = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    if (url.isEmpty) {
+      return failWidget ?? SizedBox.shrink();
+    }
+
     final w = width ?? height ?? 10;
     final h = height ?? width ?? 10;
 
@@ -41,6 +50,35 @@ class RemoteImage extends StatelessWidget {
         } else if (state.extendedImageLoadState == LoadState.completed) {
           return state.completedWidget.animate().fade();
         } else if (state.extendedImageLoadState == LoadState.failed) {
+          // If blocked by CORS, try to use Proxy
+          if (retry && kIsWeb) {
+            // Unblock CORS
+            // var fixedUrl = url;
+            // fixedUrl =
+            //     'https://api.codetabs.com/v1/proxy?quest=${Uri.encodeComponent(url)}';
+
+            // return RemoteImage(
+            //   url: fixedUrl,
+            //   retry: false,
+            //   width: width,
+            //   height: height,
+            //   alignment: alignment,
+            //   failWidget: failWidget,
+            //   loadingWidget: loadingWidget,
+            //   cacheKey: cacheKey,
+            // );
+
+            console.wtf('rendering html image');
+
+            // HTML RENDERED IMAGE
+            return ImageNetwork(
+              image: url,
+              fitWeb: BoxFitWeb.cover,
+              height: h,
+              width: w,
+            );
+          }
+
           if (failWidget != null) return failWidget;
         }
 
